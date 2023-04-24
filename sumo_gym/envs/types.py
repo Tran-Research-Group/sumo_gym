d7990f36df83a8930aa534ce3f50b8e29e251ad8
Mikihisa Yuasa <mik0915yua@outlook.com>
2023-04-24 17:45:55 -0500

    feat: add more types

--- sumo_gym/envs/types.py
+++ sumo_gym/envs/types.py
@@ -1,6 +1,7 @@
 from typing import TypeAlias
 
+import numpy as np
 from numpy import ndarray
 
 ObsDict: TypeAlias = dict[str, ndarray]
-InfoDict: TypeAlias = dict[str, ndarray]
+InfoDict: TypeAlias = dict[str, ndarray | bool | float | np.floating]
