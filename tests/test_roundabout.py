bc692323f2af453194f8e6085b83591339c8321b
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-04-24 20:35:41 -0500

    test: change bin for win

--- tests/test_roundabout.py
+++ tests/test_roundabout.py
@@ -8,7 +8,10 @@ class TestRoundaboutEnv(unittest.TestCase):
         env = RoundaboutEnv(
             20,
             300,
-            "configs/roundabout/roundabout.sumocfg",
+            r"C:\Users\mik09\Development\git\sumo_gym\sumo_gym\config\roundabout\roundabout.sumocfg",  # "./sumo_gym/config/roundabout/roundabout.sumocfg",
+            sumo_binary=r"C:\Program Files (x86)\Eclipse\Sumo\bin\sumo.exe",
+            sumo_gui_binary=r"C:\Program Files (x86)\Eclipse\Sumo\bin\sumo-gui.exe",
+            is_gui_rendered=True,
         )
         env.reset()
         for _ in range(1000):
