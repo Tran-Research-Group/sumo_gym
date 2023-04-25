10d087330625f8e4ba318bf56a924c1e329bb7a7
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-04-24 20:58:13 -0500

    chore: move main file

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from sumo_gym.envs.roundabout import RoundaboutEnv
 
-env = RoundaboutEnv(20, 300, "sumo_gym/config/roundabout/roundabout.sumocfg")
+env = RoundaboutEnv(
+    20,
+    300,
+    "sumo_gym/config/roundabout/roundabout.sumocfg",
+    sumo_binary=r"C:\Program Files (x86)\Eclipse\Sumo\bin\sumo.exe",
+    sumo_gui_binary=r"C:\Program Files (x86)\Eclipse\Sumo\bin\sumo-gui.exe",
+)
 env.reset()
 for _ in range(1000):
     observation, reward, terminated, truncated, info = env.step(
